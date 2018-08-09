374b04182cb3bfe03ac48df2301bbcc3f942fce3
ali badakhshan <abadakhshan@gmail.com>
2018-08-09 09:35:49 +0430

    V 2.0.9:Fix Cli Production Build Bug

--- src/color-picker.module.ts
+++ src/color-picker.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {ColorPickerService} from './color-picker.service';
-import {ColorPickerDirective} from './color-picker.directive';
+import {ColorPickerDirective, SliderDirective, TextDirective, DialogComponent} from './color-picker.directive';
 import {ColorPickerInputComponent} from './color-picker-input.component'
 
 @NgModule({
     imports: [CommonModule],
     providers: [ColorPickerService],
-    declarations: [ColorPickerDirective,ColorPickerInputComponent],
-    exports: [ColorPickerDirective,ColorPickerInputComponent]
+    declarations: [ColorPickerDirective,ColorPickerInputComponent,
+        SliderDirective,TextDirective, DialogComponent],
+    exports: [ColorPickerDirective,ColorPickerInputComponent],
+    entryComponents : [DialogComponent]
 })
 export class ColorPickerModule {}
